284133dc9be12234c7d38b8bc37ef97ad281bf0d
yhwcs <yhwcs@naver.com>
2021-09-17 16:18:11 +0900

    Initial Commit

---
+++ evinfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  evinfo
+//
+//  Created by yhw on 2021/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
