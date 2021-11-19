c088b2b801a77997bde928600dcb0501504c8eae
yhwcs <yhwcs@naver.com>
2021-11-19 16:19:49 +0900

    @available(iOS 13.0, *)

--- evinfo/AppDelegate.swift
+++ evinfo/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 import GoogleMaps
 
+@available(iOS 13.0, *)
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
--- evinfo/SceneDelegate.swift
+++ evinfo/SceneDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftUI
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
