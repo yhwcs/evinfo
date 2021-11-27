fb0cd15364258e883badf00edb1f30babe8843e3
yhwcs <yhwcs@naver.com>
2021-11-27 16:48:23 +0900

    change HowToUse & Membership View List style

--- evinfo/HowToUseView.swift
+++ evinfo/HowToUseView.swift
@@ -15,34 +15,35 @@ struct HowToUseView: View {
                     .font(.title3)
                     .foregroundColor(.green)
                 List{
-                Group{
-                    Text("● 회원카드")
-                        .font(.headline)
-                    Text("전기차 충전기에서 회원인증을 하기 위한 카드")
-                    Text("● 결제카드")
-                        .font(.headline)
-                    Text("충전요금을 내기 위한 카드\n(일반 신용, 체크 카드)")
-                }
-                Group{
-                    Text("● 카드 준비 절차")
-                        .font(.headline)
-                    Text("1. 충전 사업자 회원 가입")
-                    Text("2. 회원카드 발급\n    또는 기존카드를 인증용으로 등록")
-                    Text("3. 결제카드 등록")
-                }
-                Group{
-                    Text("● 충전 기본 절차")
-                        .font(.headline)
-                    Text("1. 충전 방식 선택")
-                    Text("2. 회원/비회원 인증")
-                    Text("   • 회원")
-                    Text("     회원카드 터치\n     또는 회원카드 번호 입력")
-                    Text("   • 비회원")
-                    Text("     후불 교통카드 기능을 지원하는 결제카드 터치")
-                    Text("3. 충전 플러그 꽂음")
-                    Text("4. 충전 완료 후 내역 확인")
-                    Text("5. 충전 플러그 원위치")
-                }
+                    Section(header:
+                        Text("회원카드")
+                    ){
+                        Text("전기차 충전기에서 회원인증을 하기 위한 카드")
+                    }
+                    Section(header:
+                        Text("결제카드")
+                    ){
+                        Text("충전요금을 내기 위한 카드")
+                        Text("(일반 신용, 체크 카드)")
+                    }
+                    Section(header:
+                        Text("카드 준비 절차")
+                    ){
+                        Text("1. 충전 사업자 회원 가입")
+                        Text("2. 회원카드 발급\n    또는 기존카드를 인증용으로 등록")
+                        Text("3. 결제카드 등록")
+                    }
+                    Section(header:
+                        Text("충전 기본 절차")
+                    ){
+                        Text("1. 충전 방식 선택")
+                        Text("2. 회원/비회원 인증")
+                        Text(" • 회원\n   회원카드 터치\n   또는 회원카드 번호 입력")
+                        Text(" • 비회원\n   후불 교통카드 기능을 지원하는 결제카드 터치")
+                        Text("3. 충전 플러그 꽂음")
+                        Text("4. 충전 완료 후 내역 확인")
+                        Text("5. 충전 플러그 원위치")
+                    }
                 }
             }.padding(20)
        // }
--- evinfo/MembershipView.swift
+++ evinfo/MembershipView.swift
@@ -14,15 +14,16 @@ struct MembershipView: View {
                 .font(.title3)
                 .foregroundColor(.green)
             List{
-                Group{
+                Section(header:
                     HStack{
                         Text("한국자동차환경협회(환경부)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.ev.or.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '회원가입'")
                         .font(.callout)
                     Text("회원카드: '마이페이지 > 회원카드 관리'에서 회원카드 발급 관리")
@@ -30,51 +31,57 @@ struct MembershipView: View {
                     Text("결제카드: '마이페이지 > 결제카드 관리'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("한국전기차충전서비스(해피차저)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.happecharger.co.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '회원가입'")
                         .font(.callout)
                     Text("회원카드: '회원서비스 > 멤버십정보'에서 가입 시 발급된 회원카드 확인")
                         .font(.callout)
                     Text("결제카드: '회원서비스 > 결제카드정보'에서 결제에 사용할 카드 등록")
-                        .font(.callout)
+                    .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("한국전력공사(켑코플러그)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://evc.kepco.co.kr:4445/")!,
                              label: {
                                 Image(systemName: "link")
+                                .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '로그인' → 하단의 '회원가입'")
                         .font(.callout)
                     Text("회원카드: '마이페이지 > 내정보/간편결제관리 > 충전카드'에서 인증용으로 사용할 타사 회원카드 등록")
                         .font(.callout)
                     Text("✳︎ 한전은 자사 회원카드 발급을 실시하지 않음")
                         .font(.callout)
-                    Text("'마이페이지 > 내정보/간편결제관리 > 결제카드'에서 결제에 사용할 카드 등록")
+                    Text("결제카드: '마이페이지 > 내정보/간편결제관리 > 결제카드'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                     Text("✳︎ 아파트 한전 충전기는 로밍이 안되므로 충전카드 및 결제카드 등록을 모두 마쳐야 사용 가능")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("차지비(구 포스코ICT, 현 (주)차지비)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.chargev.co.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '회원가입'")
                         .font(.callout)
                     Text("회원카드: 'MY 차지비 > 마이페이지 > 멤버십 카드'에서 가입 시 발급된 회원카드 확인")
@@ -82,15 +89,17 @@ struct MembershipView: View {
                     Text("결제카드: 'MY 차지비 > 마이페이지 > 결제용 신용카드'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("대영채비")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.chaevi.co.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '회원가입'")
                         .font(.callout)
                     Text("회원카드: '내정보 > 회원인증'에서 자사 회원카드 발급 또는 인증용으로 사용할 타사 회원카드 등록")
@@ -98,15 +107,17 @@ struct MembershipView: View {
                     Text("결제카드: '내정보 > 간편결제'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("에버온")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.everon.co.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '개인회원가입'")
                         .font(.callout)
                     Text("회원카드: '마이페이지 > 회원카드 및 결제카드 관리 > 회원카드 정보'에서 가입 시 발급된 회원카드 확인")
@@ -114,15 +125,17 @@ struct MembershipView: View {
                     Text("결제카드: '마이페이지 > 회원카드 및 결제카드 관리 > 결제정보'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("지엔텔(지차저)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://www.gcharger.net/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '개인회원가입' 또는 지차저 애플리케이션에서 가입")
                         .font(.callout)
                     Text("회원카드: 어플리케이션 'MENU > (본인이름) > 하단 회원카드 조회/발급'에서 자사 회원카드 발급")
@@ -132,15 +145,17 @@ struct MembershipView: View {
                     Text("결제카드: 어플리케이션 'MENU > 결제카드등록'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("에스트래픽(SS차저)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://sscharger.co.kr/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 '개인회원가입' 또는 지차저 어플리케이션에서 가입")
                         .font(.callout)
                     Text("회원카드: '마이페이지 > 멤버십 카드 관리'에서 가입 시 발급된 회원카드 확인")
@@ -148,15 +163,17 @@ struct MembershipView: View {
                     Text("결제카드: '마이페이지 > 개인회원 정보변경 > 신용카드'에서 결제에 사용할 카드 등록")
                         .font(.callout)
                 }
-                Group{
+
+                Section(header:
                     HStack{
                         Text("클린일렉스(K차저)")
-                            .availalbeTextModifier()
                         Link(destination: URL(string: "https://kcharger.net/")!,
                              label: {
                                 Image(systemName: "link")
+                                    .foregroundColor(.blue)
                         })
                     }
+                ){
                     Text("가입: 우측 상단 'Join' 또는 K차저 어플리케이션에서 가입")
                         .font(.callout)
                     Text("회원카드: 어플리케이션 우측 상단 아이콘 '마이페이지 > 회원로밍카드등록'에서 인증용으로 쓸 타사 회원카드 등록")
